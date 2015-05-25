4c929c816ba252d1a0de77e87f431b9084be5c52
jixiuf <jixiuf@gmail.com>
2015-05-25 10:20:24 +0800

    getclient add timeout param

--- memcache_utils.go
+++ memcache_utils.go
@@ -5,26 +5,29 @@ import (
 	"github.com/dropbox/godropbox/net2"
 	"hash/crc32"
 	"strings"
+	"time"
 )
 
-func GetClient(mcSetting string, maxActiveConnCnt int32, maxIdleConnCnt uint32, logError func(error), logInfo func(v ...interface{})) (mc memcache.Client) {
+func GetClient(mcSetting string, maxActiveConnCnt int32, maxIdleConnCnt uint32, readTimeout, writeTimeout time.Duration, logError func(error), logInfo func(v ...interface{})) (mc memcache.Client) {
 	mcAddrList := strings.Split(mcSetting, ",")
 	if len(mcAddrList) == 1 {
-		return getClientFromShardPool(mcAddrList, maxActiveConnCnt, maxIdleConnCnt, logError, logInfo)
+		return getClientFromShardPool(mcAddrList, maxActiveConnCnt, maxIdleConnCnt, readTimeout, writeTimeout, logError, logInfo)
 		// getSingleClient(mcAddrList[0])
 	} else if len(mcAddrList) == 0 { // 0 ,
 		panic("could not load mc setting,mcAddrList len 0")
 		return
 	} else { // >1
-		return getClientFromShardPool(mcAddrList, maxActiveConnCnt, maxIdleConnCnt, logError, logInfo)
+		return getClientFromShardPool(mcAddrList, maxActiveConnCnt, maxIdleConnCnt, readTimeout, writeTimeout, logError, logInfo)
 	}
 	return
 }
 
-func getClientFromShardPool(mcAddrList []string, maxActiveConnCnt int32, maxIdleConnCnt uint32, logError func(error), logInfo func(v ...interface{})) (mc memcache.Client) {
+func getClientFromShardPool(mcAddrList []string, maxActiveConnCnt int32, maxIdleConnCnt uint32, readTimeout, writeTimeout time.Duration, logError func(error), logInfo func(v ...interface{})) (mc memcache.Client) {
 	options := net2.ConnectionOptions{
 		MaxActiveConnections: maxActiveConnCnt,
 		MaxIdleConnections:   maxIdleConnCnt,
+		ReadTimeout:          readTimeout,
+		WriteTimeout:         writeTimeout,
 	}
 
 	manager := NewStaticShardManager(
